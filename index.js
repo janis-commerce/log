e093c08980f963b0a5de83b4efa472c01bbabf61
Nataniel4 <nataniel.lopez@fizzmod.com>
2019-07-24 17:57:02 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const { Log } = require('./lib');
+
+module.exports = Log;
---
+++ tests/log-test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const assert = require('assert');
+
+const sandbox = require('sinon').createSandbox();
+
+const { Log, LogError } = require('./../lib');
+
+describe('Log', () => {
+
+	// your tests here...
+
+});
